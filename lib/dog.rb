340d98e8980ddd426946cb550450c0e37463fa57
Samantha Wabi <swfullpotential@gmail.com>
2020-06-03 18:42:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,5 @@
 class Dog 
-  def initialize 
-    @name=name
-    @breed = breed
-  end
-  def name=(name)
-    @name=name
-  end
-  def name
-    @name
-  end
-  def breed=(breed)
-    @breed=breed
-  end
-  def breed
-    @breed
+  def initialize
+    
   end
 end
